895ca6024c2de1ea6e3ff1f372c9c79f5af64126
vophuhao <vophuhao2004@gmail.com>
2024-10-28 11:56:21 +0700

    search và phan trang

--- demo/src/main/java/vn/iostart/controller/admin/CategoryController.java
+++ demo/src/main/java/vn/iostart/controller/admin/CategoryController.java
@@ -66,60 +66,99 @@ public class CategoryController {
 	@RequestMapping("")
 	public String list(ModelMap model) {
 
-	List<CategoryEntity> list = categoryService.findAll();
+		List<CategoryEntity> list = categoryService.findAll();
 
-	model.addAttribute("categories", list);
-	return "admin/list";
+		model.addAttribute("categories", list);
+		return "admin/list";
 
-	/*@GetMapping("edit/{categoryId)")
-	public ModelAndView edit (ModelMap model, @PathVariable("categoryId") Long categoryId) {
-		Optional<CategoryEntity> optCategory categoryService.findById(categoryId);
-		Category Model cateHodel = new CategoryModel();
-
-		if(optCategory.isPresent()) {
-			CategoryEntity entity optCategory.get();
-
-			BeanUtils.copyProperties (entity, cateModel);
-			cateModel.setIsEdit(true);
-
-			model.addAttribute("category", cateModel);
-			return new ModelAndView("admin/categories/addOrEdit", model);
-			model.addAttribute("message", "Category is not existed!!!!"); return new ModelAndView("forward:/admin/categories", model);
-		}*/
+		/*
+		 * @GetMapping("edit/{categoryId)") public ModelAndView edit (ModelMap
+		 * model, @PathVariable("categoryId") Long categoryId) {
+		 * Optional<CategoryEntity> optCategory categoryService.findById(categoryId);
+		 * Category Model cateHodel = new CategoryModel();
+		 * 
+		 * if(optCategory.isPresent()) { CategoryEntity entity optCategory.get();
+		 * 
+		 * BeanUtils.copyProperties (entity, cateModel); cateModel.setIsEdit(true);
+		 * 
+		 * model.addAttribute("category", cateModel); return new
+		 * ModelAndView("admin/categories/addOrEdit", model);
+		 * model.addAttribute("message", "Category is not existed!!!!"); return new
+		 * ModelAndView("forward:/admin/categories", model); }
+		 */
 
 	}
-	@GetMapping("/add") public String add(ModelMap model) { 
-		
-		 return"admin/add";
-		  
-}
+
+	@GetMapping("/add")
+	public String add(ModelMap model) {
+
+		return "admin/add";
+
+	}
+
 	@PostMapping("save")
-	public String add(@Valid CategoryEntity category, BindingResult result, Model model){
-        if (result.hasErrors()) {
-                return "admin/categories/add";
-        }
-        categoryService.save(category);
-        return "redirect:/admin/categories";
+	public String add(@Valid CategoryEntity category, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			return "admin/categories/add";
+		}
+		categoryService.save(category);
+		return "redirect:/admin/categories";
+	}
+
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable("id") long id, ModelMap model) {
+		CategoryEntity category = categoryService.findById(id).orElseThrow(() -> new RuntimeException("Not found"));
+		model.addAttribute("category", category);
+		return "admin/edit";
+	}
+
+	@PostMapping("/update")
+	public String update(@Valid CategoryEntity category, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			return "admin/home";
+		}
+		categoryService.save(category);
+		return "redirect:/admin/categories";
+	}
+
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable("id") long id, Model model) {
+		categoryService.deleteById(id);
+		return "redirect:/admin/categories";
+	}
+
+	@RequestMapping("searchpaginated")
+	  public String search (ModelMap model,@RequestParam(name="name", required = false) String name,@RequestParam("page") Optional<Integer> page, @RequestParam("size") Optional<Integer> size) {
+	  int count = (int) categoryService.count();
+	  int currentPage= page.orElse(1);
+	  int pageSize= size.orElse(3);
+	  Pageable pageable= PageRequest.of (currentPage-1, pageSize, Sort.by("name"));
+	  Page<CategoryEntity> resultPage= null;
+	  if(StringUtils.hasText(name)) { 
+	resultPage = categoryService.findByNameContaining(name, pageable);
+	  model.addAttribute("name", name);}
+	  else { 
+		  resultPage = categoryService.findAll(pageable);
+	  }
+	  int totalPages = resultPage.getTotalPages();
+	  if(totalPages > 0) {
+	  int start= Math.max(1, currentPage-2);
+	  int end = Math.min(currentPage + 2, totalPages);
+	  if(totalPages > count) {
+		  
+		  if(end == totalPages) 
+			  start =end+ count;
+		  else if (start == 1) 
+			  end =start + count;
+	  }
+	  List<Integer> pageNumbers = IntStream.rangeClosed(start, end) 
+			  .boxed().collect(Collectors.toList()); 
+	  	model.addAttribute("pageNumbers", pageNumbers); 
+	  	model.addAttribute("categoryPage", resultPage); 
+	  	
+	  }
+	  return "admin/search";
+	  }
+
 }
-	 @GetMapping("/edit/{id}")
-     public String edit(@PathVariable("id") long id, ModelMap model){
-             CategoryEntity category = categoryService.findById(id)
-                     .orElseThrow(() -> new RuntimeException("Not found"));
-             model.addAttribute("category", category);
-             return "admin/edit";
-     }
-	 @PostMapping("/update")
-     public String update( @Valid CategoryEntity category, BindingResult result,
-                          Model model){
-             if (result.hasErrors()){
-                     return "admin/home";
-             }
-             categoryService.save(category);
-             return "redirect:/admin/categories";
-     }
-	  @GetMapping("/delete/{id}")
-      public String delete(@PathVariable("id") long id, Model model){
-              categoryService.deleteById(id);
-              return "redirect:/admin/categories";
-      }
-}
+
